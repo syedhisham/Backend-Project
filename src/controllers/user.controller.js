0800a00a43ed33483c593175069a99e6a1517b71
Syed Hisham Shah <syedhishamshah27@gmail.com>
2024-08-25 03:33:46 +0500

    add typos debugging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -90,7 +90,7 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 const loginUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
-  if (!email || !username) {
+  if (!email && !username) {
     throw new ApiError(400, "Username or Email is required");
   }
   const user = await User.findOne({
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
-import bcrypt, { hash } from "bcrypt";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -55,7 +55,7 @@ userSchema.pre("save", async function (next) {
   next();
 });
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await hash.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
