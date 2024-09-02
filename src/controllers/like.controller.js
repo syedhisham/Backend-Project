30117a9de6e34f98180dbc7f084e4a798e7dd5c0
Syed Hisham Shah <syedhishamshah27@gmail.com>
2024-09-02 15:04:33 +0500

    update like controller and routes

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -2,8 +2,6 @@ import { Like } from "../models/like.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { User } from "../models/user.model.js";
-import mongoose from "mongoose";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
@@ -45,5 +43,113 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
   }
 });
 
-const toggleCommentLike = asyncHandler(async (req, res) => {});
-export { toggleVideoLike };
+const toggleCommentLike = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId) {
+    throw new ApiError(401, "Enter the comment Id to proceed");
+  }
+  const userId = req.user?._id;
+  const existingCommentlike = await Like.findOne({
+    likedBy: userId,
+    comment: commentId,
+  });
+
+  if (existingCommentlike) {
+    const deleteCommentLike = await Like.findOneAndDelete({
+      _id: existingCommentlike._id,
+    });
+    if (!deleteCommentLike) {
+      throw new ApiError(
+        500,
+        "Something went wrong while deleting the comment"
+      );
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          deleteCommentLike,
+          "Comment Like deleted Successfuly"
+        )
+      );
+  }
+  if (!existingCommentlike) {
+    const likeComment = await Like.create({
+      comment: commentId,
+      likedBy: userId,
+    });
+    if (!likeComment) {
+      throw new ApiError(500, "Something went wrong while liking the comment");
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, likeComment, "Liked the comment successfuly"));
+  }
+});
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  if (!tweetId) {
+    throw new ApiError(401, "Enter the tweet Id to proceed");
+  }
+  const userId = req.user?._id;
+  const tweetLike = await Like.findOne({
+    likedBy: userId,
+    tweet: tweetId,
+  });
+  if (tweetLike) {
+    const deleteTweetLike = await Like.findOneAndDelete({
+      _id: tweetLike._id,
+    });
+    if (!deleteTweetLike) {
+      throw new ApiError(
+        500,
+        "Something went wrong while deleting the comment"
+      );
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          deleteTweetLike,
+          "Deleted the tweet like successfuly"
+        )
+      );
+  }
+  if (!tweetLike) {
+    const likeTheTweet = await Like.create({
+      likedBy: userId,
+      tweet: tweetId,
+    });
+    if (!likeTheTweet) {
+      throw new ApiError(500, "Something went wrong while liking the tweet");
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, likeTheTweet, "Liked the Tweet successfuly"));
+  }
+});
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  if (!userId) {
+    throw new ApiError(401, "Enter the user Id to get all it's liked videos");
+  }
+  const userLikedVideos = await Like.find({
+    likedBy: userId,
+  });
+  if (!userLikedVideos) {
+    throw new ApiError(
+      500,
+      "Something went wrong while fetching all the liked videos by the user"
+    );
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, userLikedVideos, "Fethced all the liked videos")
+    );
+});
+export { toggleVideoLike, toggleCommentLike, toggleTweetLike, getLikedVideos };
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import {
+  getLikedVideos,
+  toggleCommentLike,
+  toggleTweetLike,
+  toggleVideoLike,
+} from "../controllers/like.controller.js";
 
 const router = Router();
 
 router.route("/video-likes/:videoId").post(verifyJWT, toggleVideoLike);
+router.route("/comment-likes/:commentId").post(verifyJWT, toggleCommentLike);
+router.route("/tweet-likes/:tweetId").post(verifyJWT, toggleTweetLike);
+router.route("/liked-videos/:userId").get(verifyJWT, getLikedVideos);
 
 export default router;
